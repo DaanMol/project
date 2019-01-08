848c97d16b3462c5ee0b36f493ecb90899f12de6
DaanMol <Daan.molleman@Gmail.com>
2019-01-08 15:24:49 +0100

    csv to json converter

---
+++ scripts/converter.py
@@ -0,0 +1,60 @@
+# Daan Molleman
+# 11275820
+
+"""
+This script converts a csv file to a JSON file
+"""
+
+import json as j
+import csv
+
+presidents = ["Roosevelt", "Truman", "Eisenhower", "Kennedy", "Johnson", "Nixon", "Ford", \
+               "Carter", "Raegan", "BushSr", "Clinton", "BushJr", "Obama", "Trump"]
+
+def parseApproval(reader):
+    """
+    parses csv into dict
+    """
+
+    approval = {}
+    for row in reader:
+        approval.update({row["Start Date"]: {"Start": row["Start Date"], "End": row["End Date"],
+                                            "Approving": row["Approving"], "Disapproving": row["Disapproving"],
+                                            "Unsure": row["Unsure/NoData"]}})
+    return approval
+
+def parseCongress(reader):
+
+    congress = {}
+    for row in reader:
+        years = row["\ufeffCongress (Years)"].split("(")[1].replace(")", "").replace("\u2013", "-").replace("\u00a0", "")
+        congress.update({years: {"Total": row["# of House Seats"][0:3],
+                        "Democrats": row["Democrats"], "Republicans": row["Republicans"]}})
+
+    return congress
+
+def json(parsed, name):
+    """
+    Converts a dictionary to a json file
+    """
+
+    with open(f"{name}.json", "w") as write_file:
+        j.dump(parsed, write_file, indent=4)
+
+if __name__ == '__main__':
+
+    final = {}
+
+    for president in presidents:
+        with open(f"../data/approval/{president}.csv") as file:
+            reader = csv.DictReader(file)
+            parsed = parseApproval(reader)
+            final.update({president: parsed})
+
+    json(final, "presidents")
+
+    seats = {}
+    with open("../data/seats/congress.csv", encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        parsed = parseCongress(reader)
+        json(parsed, "congress")
